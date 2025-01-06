a610cdfc420ed7bf71c7d670cd49e1701e61e925
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2025-01-06 22:37:25 +0100

    Delete text.js

--- text.js
+++
@@ -1,8 +0,0 @@
-const element_Content = "text";
-const element_Decoration = "line";
-const element_Content_Target = 1;
-const element_Href_Target = "2";
-const element_Href_Type = null;
-
-console.log(element_Content); // "text"
-console.log(element_Decoration); // "line"
